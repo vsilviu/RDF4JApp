2f77764692a2e63c09ff225367c559af6453b9e3
Silviu <xsylver@yahoo.com>
2017-06-03 00:21:38 +0300

    Initial commit

---
+++ src/main/java/com/xsylver/RDF4JConfig.java
@@ -0,0 +1,10 @@
+package com.xsylver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class RDF4JConfig {
+
+
+}
---
+++ src/main/java/com/xsylver/Rdf4JAppApplication.java
@@ -0,0 +1,16 @@
+package com.xsylver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rdf4JAppApplication {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(Rdf4JAppApplication.class, args);
+//	}
+
+    public static void main(String[] args) {
+
+    }
+}
